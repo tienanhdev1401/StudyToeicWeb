947c803a85f8edf450ea7cff288b9781155c0f07
HungAnh <phanhunganh2004@gmail.com>
2025-03-09 20:09:19 +0700

    Cập nhật Header, Footer cho ToeicTestPageToeicTestPage

--- client/src/pages/test-online-new.jsx
+++ client/src/pages/test-online-new.jsx
@@ -40,7 +40,65 @@ const ToeicTest = () => {
   ];
 
   return (
-   
+    <>
+    <header 
+            style={{
+                backgroundImage: "url('/assets/img/hero/hero2.png')",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat"
+            }}
+        >
+            {/* Header Start */}
+            <div className="header-area">
+                <div className="main-header">
+                    <div className="header-bottom header-sticky">
+                        <div className="container-fluid">
+                            <div className="row align-items-center">
+                                {/* Logo */}
+                                <div className="col-xl-2 col-lg-2">
+                                    <div className="logo">
+                                        <a href="/"><img src="assets/img/logo/logo.png" alt=""/></a>
+                                    </div>
+                                </div>
+                                <div className="col-xl-10 col-lg-10">
+                                    <div className="menu-wrapper d-flex align-items-center justify-content-end">
+                                        {/* Main-menu */}
+                                        <div className="main-menu d-none d-lg-block">
+                                            <nav>
+                                                <ul id="navigation">                                                                                          
+                                                    <li className="active"><a href="/">Home</a></li>
+                                                    <li><a href="/courses">Courses</a></li>
+                                                    <li><a href="/about">About</a></li>
+                                                    <li><a href="#">Blog</a>
+                                                        <ul className="submenu">
+                                                            <li><a href="/blog">Blog</a></li>
+                                                            <li><a href="/blog_details">Blog Details</a></li>
+                                                            <li><a href="/elements">Element</a></li>
+                                                        </ul>
+                                                    </li>
+                                                    <li><a href="/contact">Contact</a></li>
+                                                    {/* Button */}
+                                                    <li className="button-header margin-left"><a href="/test-online-new" className="btn">Join</a></li>
+                                                    <li className="button-header"><a href="/login" className="btn btn3">Log in</a></li>
+                                                </ul>
+                                            </nav>
+                                        </div>
+                                    </div>
+                                </div> 
+                                {/* Mobile Menu */}
+                                <div className="col-12">
+                                    <div className="mobile_menu d-block d-lg-none"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* Header End */}
+        </header>
+
+  
     <div className="toeic-container">
        <div id="preloader-active">
                 <div class="preloader d-flex align-items-center justify-content-center">
@@ -51,7 +109,8 @@ const ToeicTest = () => {
                         </div>
                     </div>
                 </div>
-            </div>    
+            </div>
+           
       <div className="tab-buttons">
         <button 
           className={`tab ${activeTab === 'listening' ? 'active' : ''}`}
@@ -102,6 +161,10 @@ const ToeicTest = () => {
         ))}
       </div>
     </div>
+    <Footer></Footer>
+    </>
+   
+    
   );
 };
 
