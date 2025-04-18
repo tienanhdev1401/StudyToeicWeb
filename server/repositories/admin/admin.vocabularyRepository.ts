7cd504aa15e8795fae17dff40efdacd71f4b73b6
TranPhanTienAnh <tptienanh1401@gmail.com>
2025-04-18 14:44:18 +0700

    gộp code hiếu nghĩa

---
+++ server/repositories/admin/admin.vocabularyRepository.ts
@@ -0,0 +1,37 @@
+import database from '../../config/db';
+import { Vocabulary } from '../../models/Vocabulary';
+
+export class VocabularyRepository {
+    static async countVocabulariesByTopicId(topicId: Number): Promise<number> {
+        const results = await database.query(
+            'SELECT COUNT(*) as count FROM vocabularies WHERE VocabularyTopicId = ?',
+            [topicId]
+        );
+        return results[0].count;
+    }
+
+    static async getVocabulariesByTopicId(topicId: number): Promise<Vocabulary[] | null> {
+        const [results] = await database.query(
+            'SELECT * FROM vocabularies WHERE VocabularyTopicId = ?',
+            [topicId]
+        );
+    
+        if (!Array.isArray(results) || results.length === 0) {
+            return null;
+        }
+    
+        const vocabularies = results.map((row: any) => new Vocabulary(
+            row.id,
+            row.content,
+            row.meaning,
+            row.synonym,
+            row.transcribe,
+            row.urlAudio,
+            row.urlImage,
+            row.VocabularyTopicId
+        ));
+    
+        return vocabularies;
+    }
+    
+}
