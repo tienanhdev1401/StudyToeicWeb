81859352bd7add301747c07631dafb8c8cfb02bc
TranPhanTienAnh <tptienanh1401@gmail.com>
2025-04-18 14:57:49 +0700

    gộp code hiếu nghĩa

--- client/src/pages/Admin/adminpage.jsx
+++ client/src/pages/Admin/adminpage.jsx
@@ -5,60 +5,8 @@ import Sidebar from '../../components/Sidebar';
 import styles from '../../styles/AdminPage.module.css';
 import Dashboard from './Dashboard';
 import ManageVocabularyTopic from './ManageVocabularyTopic';
-<<<<<<< HEAD
-import ManageGrammarTopic from './ManageGrammarTopic';
-import ManageLearner from './ManageLearner';
-import ManageStaff from './ManageStaff';
-const AdminPage = () => {
-  const [currentPage, setCurrentPage] = useState('dashboard');
 
-  const renderPage = () => {
-    switch (currentPage) {
-      case 'dashboard':
-        return (
-          <div className={styles.contentArea}>
-            <Dashboard />
-          </div>
-        );
-        case 'vocabulary':
-          return (
-            <div className={styles.contentArea}>
-              <ManageVocabularyTopic />
-            </div>
-          );
-        case 'grammar': 
-          return (
-            <div className={styles.contentArea}>
-               <ManageGrammarTopic />
-            </div>
-          );
-        case 'exercise':
-          return (
-            <div className={styles.contentArea}>
-              <h1>Exercise Management</h1>
-              {/* Thêm component quản lý bài tập ở đây */}
-            </div>
-          );
-          case 'learner':
-          return (
-            <div className={styles.contentArea}>
-               <ManageLearner />
-            </div>
-          );
-          case 'staff':
-          return (
-            <div className={styles.contentArea}>
-               <ManageStaff />
-            </div>
-          );
-      default:
-        return (
-          <div className={styles.contentArea}>
-            <Dashboard />
-          </div>
-        );
-    }
-  };
+
 
 const AdminLayout = ({ children }) => {
   return (
