a79d6dc72bb48c4fbeae06cc943a50856fc69140
TranPhanTienAnh <tptienanh1401@gmail.com>
2025-05-12 23:51:00 +0700

    cập nhật user

--- server/models/User.ts
+++ server/models/User.ts
@@ -1,17 +1,17 @@
 import bcrypt from 'bcrypt';
 export class User {
-  public email: string;
-  public password: string;
-  public fullName?: string;
-  public role: string;
-  public id?: number;
-  public phoneNumber?: string;
-  public dateOfBirth?: Date;
-  public gender?: string;
-  public avatar?: string;
-  public joinAt?: Date;
-  public status?: string;
-  public updatedAt?: Date;
+  email: string;
+  password: string;
+  fullName?: string;
+  role: string;
+  id?: number;
+  phoneNumber?: string;
+  dateOfBirth?: Date;
+  gender?: string;
+  avatar?: string;
+  joinAt?: Date;
+  status?: string;
+  updatedAt?: Date;
 
   constructor(userData: {
     email: string;
@@ -33,23 +33,23 @@ export class User {
     this.role = userData.role || 'user';
     this.id = userData.id;
     this.phoneNumber = userData.phoneNumber;
-    
+
     // Xử lý Date
-    this.dateOfBirth = userData.dateOfBirth instanceof Date 
-      ? userData.dateOfBirth 
+    this.dateOfBirth = userData.dateOfBirth instanceof Date
+      ? userData.dateOfBirth
       : userData.dateOfBirth ? new Date(userData.dateOfBirth) : undefined;
-    
+
     this.gender = userData.gender;
     this.avatar = userData.avatar;
-    
-    this.joinAt = userData.joinAt instanceof Date 
-      ? userData.joinAt 
+
+    this.joinAt = userData.joinAt instanceof Date
+      ? userData.joinAt
       : userData.joinAt ? new Date(userData.joinAt) : undefined;
-    
+
     this.status = userData.status;
-    
-    this.updatedAt = userData.updatedAt instanceof Date 
-      ? userData.updatedAt 
+
+    this.updatedAt = userData.updatedAt instanceof Date
+      ? userData.updatedAt
       : userData.updatedAt ? new Date(userData.updatedAt) : undefined;
   }
 
